85e8985af223ef1a32df2f4304db0fd34c11f156
Gregory Guths <guthsgregory@gmail.com>
2026-03-08 18:45:13 -0300

    .

--- ExceptionHandling/Exercice_1/src/applicaton/Main.java
+++ ExceptionHandling/Exercice_1/src/applicaton/Main.java
@@ -9,35 +9,35 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Locale.setDefault(Locale.US);
-        Scanner input = new Scanner(System.in);
+        try (Scanner input = new Scanner(System.in)) {
+            try {
+                System.out.println("Enter account data: ");
 
-        try {
-            System.out.println("Enter account data: ");
+                System.out.print("Number: ");
+                int number = input.nextInt();
+                input.nextLine();
 
-            System.out.print("Number: ");
-            int number = input.nextInt();
-            input.nextLine();
+                System.out.print("Holder: ");
+                String holder = input.nextLine();
 
-            System.out.print("Holder: ");
-            String holder = input.nextLine();
+                System.out.print("Initial balance: ");
+                Double initialBalance = input.nextDouble();
 
-            System.out.print("Initial balance: ");
-            Double initialBalance = input.nextDouble();
+                System.out.print("Withdraw limit: ");
+                Double withdrawlimit = input.nextDouble();
 
-            System.out.print("Withdraw limit: ");
-            Double withdrawlimit = input.nextDouble();
+                Account account = new Account(number, holder, initialBalance, withdrawlimit);
 
-            Account account = new Account(number, holder, initialBalance, withdrawlimit);
+                System.out.println();
+                System.out.print("Enter amount for withdraw: ");
+                account.withdraw(input.nextDouble());
 
-            System.out.println();
-            System.out.print("Enter amount for withdraw: ");
-            account.withdraw(input.nextDouble());
-
-            System.out.println("New balance: " + account.getBalance());
-        } catch (DomainExpedicion e) {
-            System.out.println("Withdraw error: " + e.getMessage());
-        } catch (RuntimeException e) {
-            System.out.println("Unexpected error");
+                System.out.println("New balance: " + account.getBalance());
+            } catch (DomainExpedicion e) {
+                System.out.println("Withdraw error: " + e.getMessage());
+            } catch (RuntimeException e) {
+                System.out.println("Unexpected error");
+            }
         }
     }
 }
